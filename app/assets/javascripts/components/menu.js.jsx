bd9735d2add27fa3a60c442a2e885bc4746a5ccc
Bryan MacDiarmid <brymac@gmail.com>
2015-10-09 12:44:15 -0400

    Menu changes

--- app/assets/javascripts/components/menu.js.jsx
+++ app/assets/javascripts/components/menu.js.jsx
@@ -10,18 +10,9 @@ var Menu = React.createClass({
             <li className="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
           </ul>
           <section className="top-bar-section">
-            <ul className="right">
-              <li className="active"><a href="#">Right Button Active</a></li>
-              <li className="has-dropdown">
-                <a href="#">Right Button Dropdown</a>
-                <ul className="dropdown">
-                  <li><a href="#">First link in dropdown</a></li>
-                  <li className="active"><a href="#">Active link in dropdown</a></li>
-                </ul>
-              </li>
-            </ul>
             <ul className="left">
-              <li><a href="#">Left Nav Button</a></li>
+              <li><a href="/">Time Entries</a></li>
+              <li><a href="/projects/">Projects</a></li>
             </ul>
           </section>
         </nav>

fa4e9500b8b5dd3143c452d023e3219b66290d96
Bryan MacDiarmid <brymac@gmail.com>
2015-10-17 17:02:55 -0400

    No project no longer results in '0' value

--- app/assets/javascripts/components/select_box.js.jsx
+++ app/assets/javascripts/components/select_box.js.jsx
@@ -8,7 +8,7 @@ function logChange(val) {
 var SelectBox = React.createClass({
   render: function() {
     var options = this.props.options
-    options.unshift({value: null, label: 'Select a project (optional)'})
+    options.unshift({value: '', label: 'Select a project (optional)'})
     var selectOptions = [];
     for (var i=0; i < options.length; i++) {
       selectOptions.push(<option value={options[i]['value']} key={i} onChange={this.handleChange}>{options[i]['label']}</option>);

394a737a2f384178fd4fe4b8c01aec746e917bd5
Bryan MacDiarmid <brymac@gmail.com>
2015-10-17 16:57:59 -0400

    Form now updating project ID, although no project updates to zero instead of null

--- app/assets/javascripts/components/select_box.js.jsx
+++ app/assets/javascripts/components/select_box.js.jsx
@@ -11,7 +11,7 @@ var SelectBox = React.createClass({
     options.unshift({value: null, label: 'Select a project (optional)'})
     var selectOptions = [];
     for (var i=0; i < options.length; i++) {
-      selectOptions.push(<option value="{options[i]['value']}" key={i} onChange={this.handleChange}>{options[i]['label']}</option>);
+      selectOptions.push(<option value={options[i]['value']} key={i} onChange={this.handleChange}>{options[i]['label']}</option>);
     }
 
     return (
